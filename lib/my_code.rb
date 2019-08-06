90f18ebe43a25c45602d4117135feaf14cfa4fc9
Adam Finch <atyreefinch@gmail.com>
2019-08-06 02:20:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+def map (arr)
+   arr2=[]
+   i=0
+  while i < arr.length do
+  arr2.push(yield(arr[i]))
+   i+=1
+  end
+  arr2
+end
+
+def reduce (arr,starting_value=nil)
+  if starting_value
+    adder=starting_value
+    i=0 
+  else
+    adder=arr[0] 
+    i=1
+  end
+  while i < arr.length do
+      adder=yield(adder,arr[i])
+      i+=1 
+  end
+  adder
+end
+  
+      
+
